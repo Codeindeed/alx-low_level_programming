f6edba0d4b23359eafbf89687a8c5f2c69be3399
Codeindeed <favouralex084@gmail.com>
2022-03-14 01:45:47 -0700

    nuber with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print numbers with , and " "
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+		if (x != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
