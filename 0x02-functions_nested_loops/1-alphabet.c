5f2e83430eea3bef9c04a8e8b42bc505a6817c64
Codeindeed <favouralex084@gmail.com>
2022-03-15 16:47:51 -0700

    main

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,10 @@
  * Return: 0
  */
 
-void print_alphabelt(void);
-
-int main(void)
-{
-	print_alphabelt();
-	return (0);
-}
 void print_alphabelt(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
