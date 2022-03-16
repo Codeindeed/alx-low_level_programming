d67ff1a0b9fbd401048f1b9ef09f647cdc40b00d
Codeindeed <favouralex084@gmail.com>
2022-03-16 15:26:48 -0700

    seconds

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - Time spent by jack bauer
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i, j, k, m, t, n;
+
+	for (i = 0; i < 24; i++)
+	{
+		k = i / 10;
+		n = i % 10;
+
+		for (j = 0; j < 60; j++)
+		{
+			m = j / 10;
+			n = j % 10;
+
+			_putchar('0' + k);
+			_putchar('0' + n);
+			_putchar(':');
+			_putchar('0' + m);
+			_putchar('0' + n);
+			_putchar('\n');
+		}
+	}
+}
