7e2b39370cb20e57c09fb84e0cc57efa376dc820
Codeindeed <favouralex084@gmail.com>
2022-03-16 04:46:19 -0700

    check for upper and lowercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks for capital and lowercase letters
+ * @c: character
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
