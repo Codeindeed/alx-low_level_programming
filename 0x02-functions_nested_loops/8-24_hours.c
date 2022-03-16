97d6c27a1393e4439aaa1718b1bbdada1a27ba29
Codeindeed <favouralex084@gmail.com>
2022-03-16 15:39:30 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, m, n;
+	int i, j, k, m, n, t;
 
 	for (i = 0; i < 24; i++)
 	{
@@ -15,13 +15,13 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			m = j / 10;
-			n = j % 10;
+			t = j % 10;
 
 			_putchar('0' + k);
 			_putchar('0' + n);
 			_putchar(':');
 			_putchar('0' + m);
-			_putchar('0' + n);
+			_putchar('0' + t);
 			_putchar('\n');
 		}
 	}
