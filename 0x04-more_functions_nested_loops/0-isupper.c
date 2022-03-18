145b3c5b12e2025c77b311291de26f2936087dbb
Codeindeed <favouralex084@gmail.com>
2022-03-18 03:36:27 -0700

    done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isupper- checks for upper and lower case letters:
- * @c: is a character
+ * _isupper- checks for uppercase
+ * @c: is an integer
  * Return: 0
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if ((c >= 65) && (c <= 90))
 	{
 		return (1);
 	}
