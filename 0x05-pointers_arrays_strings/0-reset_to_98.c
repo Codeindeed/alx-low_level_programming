8f241e0174967d91389398fec342e0471477c602
Codeindeed <favouralex084@gmail.com>
2022-03-21 10:59:31 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * rest_to_98 - Resets to pointer value in the function
+ * reset_to_98 - Resets to pointer value in the function
  * @n: pointer
  * Return: void 
  */
-void rest_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
