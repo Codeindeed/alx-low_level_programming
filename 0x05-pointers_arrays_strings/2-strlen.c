ac958f22899c6a650fa313d27c2893adf3e9cd6c
Codeindeed <favouralex084@gmail.com>
2022-03-22 14:35:06 -0700

    done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the number of strings
+ * @*s: char 
+ * Return: 0 
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
